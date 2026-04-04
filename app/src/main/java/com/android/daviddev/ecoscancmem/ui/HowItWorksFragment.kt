83cad9b100f4693d58bb0121edbc56236ee096eb
DavidCarvalhido <davidcarvalhido@hotmail.com>
2026-04-04 19:02:29 +0100

    build: implementation of a Splash that describes the app features

---
+++ app/src/main/java/com/android/daviddev/ecoscancmem/ui/HowItWorksFragment.kt
@@ -0,0 +1,7 @@
+package com.android.daviddev.ecoscancmem.ui
+
+import androidx.fragment.app.Fragment
+import com.android.daviddev.ecoscancmem.R
+
+class HowItWorksFragment : Fragment(R.layout.fragment_how_it_works) {
+}
